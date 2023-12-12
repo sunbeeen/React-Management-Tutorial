d4383354939c4baadddfbb391dd9434cb9d41fc4
sunbeen0111@naver.com <sunbeen0111@naver.com>
2023-12-13 00:25:11 +0900

    Create Customer js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://img.animalplanet.co.kr/news/2019/09/30/700/2kib2lrqm0nqxw76il2d.jpg',
+  'name': '박선빈',
+  'birthday': '961222',
+  'gender':'남자',
+  'job':'대학생'
+ },
+ {
+  'id': 2,
+  'image': 'https://img.animalplanet.co.kr/news/2019/09/30/700/2kib2lrqm0nqxw76il2d.jpg',
+  'name': '홍길동',
+  'birthday': '010111',
+  'gender':'남자',
+  'job':'백수'
+ },
+ {
+  'id': 3,
+  'image': 'https://img.animalplanet.co.kr/news/2019/09/30/700/2kib2lrqm0nqxw76il2d.jpg',
+  'name': '박선방',
+  'birthday': '900111',
+  'gender':'여자',
+  'job':'대학생'
+ }
+]
+class App extends Component {
+  render(){
+    return (
+      <div>
+      {customers.map(c=>{return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />);})}
+      </div>
+    );
+  }
 }
 
 export default App;
